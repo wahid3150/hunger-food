7747642f26e64edf6f56a7575ac3c9a3ee3b5298
Wahid ali <wahidali3150@gmail.com>
2026-04-03 15:30:37 +0500

    Add database connection setup and integrate with server startup

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
-import dotenv from "dotenv";
+import "dotenv/config";
 import express from "express";
+import connectDB from "./config/db.js";
 
-dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`server is running on ${PORT}`);
 });
