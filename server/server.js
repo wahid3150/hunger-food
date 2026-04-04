b86501ea8818a8f8c5bae866b9c870618cbdefbc
Wahid ali <wahidali3150@gmail.com>
2026-04-04 10:15:40 +0500

    Set up Express server with middleware for JSON parsing, cookie handling, CORS, and authentication routes

--- server/server.js
+++ server/server.js
@@ -1,8 +1,21 @@
 import "dotenv/config";
 import express from "express";
 import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
+import router from "./routes/authRouter.js";
+import cors from "cors";
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+app.use("/api/auth", router);
 
 const PORT = process.env.PORT || 5000;
 
