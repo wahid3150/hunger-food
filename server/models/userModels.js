1b5e55416f53ace133c4607991d4f3ea25ee406a
Wahid ali <wahidali3150@gmail.com>
2026-04-04 17:23:42 +0500

    Add field for otp send and password reset

--- server/models/userModels.js
+++ server/models/userModels.js
@@ -31,6 +31,16 @@ const userSchema = new mongoose.Schema(
       enum: ["user", "owner", "deliveryBoy"],
       required: true,
     },
+    resetOtp: {
+      type: String,
+    },
+    isOtpVerified: {
+      type: Boolean,
+      default: false,
+    },
+    otpExpires: {
+      type: Date,
+    },
   },
   { timestamps: true },
 );
