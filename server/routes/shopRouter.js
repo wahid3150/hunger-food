ca3aadce2a78a5d89435a4427634144609d3bc46
Wahid ali <wahidali3150@gmail.com>
2026-04-08 10:50:30 +0500

    Add middleware for image upload

--- server/routes/shopRouter.js
+++ server/routes/shopRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 import isAuth from "../middleware/isAuth.js";
 import { createShop, updateShop } from "../controller/shopController.js";
+import { upload } from "../middleware/multer.js";
 
 const router = express.Router();
 
-router.post("create-shop", isAuth, createShop);
-router.post("update-shop", isAuth, updateShop);
+router.post("/create-shop", isAuth, upload.single("image"), createShop);
+router.put("/update-shop/:shopId", isAuth, upload.single("image"), updateShop);
 
 export default router;
