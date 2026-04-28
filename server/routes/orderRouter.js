a74caa217024f7e1cdb14c6d70356a64bb9e7bb6
Wahid ali <wahidali3150@gmail.com>
2026-04-28 14:05:11 +0500

    Order route added for user view

--- server/routes/orderRouter.js
+++ server/routes/orderRouter.js
@@ -10,6 +10,7 @@ import {
 const router = express.Router();
 
 router.post("/", isAuth, createOrder);
+router.get("/my-orders", isAuth, getMyOrders);
 router.post("/my-orders", isAuth, getMyOrders);
 router.get("/shop/:shopId", isAuth, getShopOrders);
 router.patch("/:orderId/status", isAuth, updateOrderStatus);
